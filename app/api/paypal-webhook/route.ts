7e2417737402fd6411c72347d41624143ab4a56c
tsadjioulrich@gmail.com <tsadjioulrich@gmail.com>
2024-09-17 00:28:13 -1100

    webhook

--- app/api/paypal-webhook/route.ts
+++ app/api/paypal-webhook/route.ts
@@ -1,5 +1,5 @@
 import crypto from "crypto"
-import crc32 from "crc/crc32"
+import * as crc32 from "buffer-crc32"
 import { headers } from 'next/headers'
 import { NextResponse } from "next/server";
 
@@ -13,7 +13,7 @@ export async function POST(req:Request){
     const paypaltxsig =  headers().get("paypal-transmission-sig")
     const event = await req.text()
     
-    const crc = parseInt("0x" + crc32(event).toString());
+    const crc = parseInt("0x" + crc32(event).toString('hex'));
     const message = `${transmissionId}|${timeStamp}|${WEBHOOK_ID}|${crc}`
     console.log("original signed message",message)
     const certPem = await downloadAndCache(paypalcerturl!);
